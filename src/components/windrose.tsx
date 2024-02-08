260650e51086c0e51738d92e15b8cbd1c772c7e7
Vetle Hofsøy-Woie <vetle.hofsoy-woie@sintef.no>
2024-02-08 14:28:02 +0100

    :lipstick: Remove axisline and smaller gap between bins in windrose

--- src/components/windrose.tsx
+++ src/components/windrose.tsx
@@ -5,25 +5,28 @@ export const Windrose = (props: { angleBins: string[], radialBins: string[], dat
         angleAxis: {
             type: 'category',
             startAngle: 90 + 360 / (2 * props.angleBins.length),
-            data: props.angleBins
+            data: props.angleBins,
+            splitArea: {show:true},
         },
         radiusAxis: {
-            axisLabel: { show: false },
-            axisPointer: { show: false }
+            axisLabel: {show:false},
+            axisPointer: {show:false},
+            axisLine:{show:false}
         },
         polar: {
         },
         series: props.radialBins.map((radialbin, idx) => {
             return (
                 {
-                    type: 'bar',
-                    data: props.data[idx],
-                    coordinateSystem: 'polar',
-                    name: radialbin,
-                    stack: 'a',
-                    emphasis: {
-                        focus: 'series'
-                    }
+                type: 'bar',
+                data: props.data[idx],
+                coordinateSystem: 'polar',
+                name: radialbin,
+                barWidth: "95%",
+                stack: 'a',
+                emphasis: {
+                  focus: 'series'
+                  }
                 }
             )
         }),
